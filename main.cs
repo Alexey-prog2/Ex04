97d6a99052adf61ca646690946327607b047512d
gameboyadvanceh <gameboyadvancehey@gmail.com>
2021-03-06 16:20:20 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,8 @@
+using System;
+
+class MainClass {  public static void Main (string[] args) {
+    Console.WriteLine ("Please enter username");
+    string userName = Console.ReadLine();
+    Console.WriteLine("Username is: " + userName);
+  }
+}
